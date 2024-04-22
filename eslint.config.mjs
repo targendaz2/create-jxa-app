ff7554ee79256d215561e10184215124b1930c54
David G Rosenberg <targendaz2@me.com>
2024-04-22 06:08:44 -0400

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,60 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+
+const config = tseslint.config(
+  {
+    ignores: ['dist/*'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    files: ['src/**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-var-requires': 'off',
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: ['node:*'],
+        },
+      ],
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector: 'AwaitExpression',
+          message: 'JXA does not support async JavaScript.',
+        },
+        {
+          selector: 'ExportDefaultDeclaration',
+          message: 'Webpack breaks default exports in JXA code.',
+        },
+        {
+          selector: 'SpreadElement',
+          message: 'JXA does not support the spread operator.',
+        },
+      ],
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      eqeqeq: 'error',
+      'no-var': 'error',
+    },
+  },
+);
+
+export default config;
---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import { JestConfigWithTsJest, pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+const config: JestConfigWithTsJest = {
+  moduleFileExtensions: ['js', 'ts'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>',
+    useESM: true,
+  }),
+  preset: 'ts-jest',
+  setupFilesAfterEnv: ['./tests/extensions/fs.ext.ts'],
+  testEnvironment: 'node',
+  testRegex: '.+\\.test\\.ts$',
+  testTimeout: 10000,
+};
+
+export default config;
